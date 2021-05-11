d04e7e168af777eb6081cd8f4105afb790170966
nomin.sasabuchi <nomin.sasabuchi@karabiner.tech>
2021-05-11 12:28:00 +0900

    微修正

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    ],
+  },
+};
